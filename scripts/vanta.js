4d0f966f6aa6bfd9578bed6c8dc2c30e2650daea
acdavi <daviaraujocandido@gmail.com>
2022-09-28 15:28:38 -0300

    MyLink

---
+++ scripts/vanta.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+    VANTA.NET ({
+        el: "#vanta",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x16abf2,
+        backgroundColor: 0x1c1237,
+        backgroundAlpha: 0.5,
+        maxDistance: 60.00,
+        spacing: 50.00
+    })
+})
+
