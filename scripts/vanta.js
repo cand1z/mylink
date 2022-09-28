623e2ba3ea7c811f548b117344b6888bdc017980
acdavi <daviaraujocandido@gmail.com>
2022-09-28 20:10:43 -0300

    datails

--- scripts/vanta.js
+++ scripts/vanta.js
@@ -2,15 +2,15 @@ window.addEventListener('DOMContentLoaded', ()=>{
     VANTA.NET ({
         el: "#vanta",
         mouseControls: true,
-        touchControls: true,
+        touchControls: false,
         gyroControls: false,
         minHeight: 200.00,
         minWidth: 200.00,
-        scale: 1.00,
-        scaleMobile: 1.00,
+        scale: 1.0,
+        scaleMobile: 1.0,
         color: 0x16abf2,
         backgroundColor: 0x1c1237,
-        backgroundAlpha: 0.5,
+        backgroundAlpha: 0.0,
         maxDistance: 60.00,
         spacing: 50.00
     })
