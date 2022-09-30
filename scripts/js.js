ed8636294b19b8cba6ec6f466aaa4f347c67a341
acdavi <daviaraujocandido@gmail.com>
2022-09-30 17:39:22 -0300

    transiction for one item

--- scripts/js.js
+++ scripts/js.js
@@ -1,5 +1,5 @@
-setTimeout( () => {
-    const main = document.querySelector('main')
-    main.style.opacity = 1
-},1000)
+const element = document.getElementById("teste")
+console.log("teste", element)
+element.style.opacity = 1
+element.style.filter = "blur(0px)"
 
