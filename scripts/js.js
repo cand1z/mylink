226de7cbb726f6465c70438617a8336598222d07
acdavi <daviaraujocandido@gmail.com>
2022-10-01 01:40:05 -0300

    changing background and ajusting css/js

--- scripts/js.js
+++ scripts/js.js
@@ -1,6 +1,10 @@
 setTimeout(() => {
-    const element = document.querySelector("ul")
-    const elements = element.querySelectorAll("li")
-    element.style.opacity = 1
+    const element = document.querySelectorAll("li")
+    console.log("elements", element.length)
+    for (let i = 0; i < element.length; i++){
+        element[i].style.opacity = 1
+        element[i].style.filter = "blur(0px)"
+        element[i].style.backdropFilter = "blur(10px)"
+    }
 }, 1000)
 
