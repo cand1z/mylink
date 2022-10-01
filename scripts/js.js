1cce985b2606dae32d7ed58684894bacdd5bd7d0
acdavi <daviaraujocandido@gmail.com>
2022-09-30 22:08:52 -0300

    setting timer to blur transition

--- scripts/js.js
+++ scripts/js.js
@@ -1,4 +1,6 @@
-const element = document.getElementById("blur")
-element.style.opacity = 1
-element.style.filter = "blur(0px)"
+setTimeout(() => {
+    const element = document.getElementById("blur")
+    element.style.opacity = 1
+    element.style.filter = "blur(0px)"
 
+}, 1000)
